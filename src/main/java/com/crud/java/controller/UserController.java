60e7dd7a4c2c4f10d476de7f027f3f3dd6d09ad7
Kan Sophan <ytbro65@gmail.com>
2023-08-10 19:07:24 +0700

    last time

--- src/main/java/com/crud/java/controller/UserController.java
+++ src/main/java/com/crud/java/controller/UserController.java
@@ -15,44 +15,38 @@ import java.util.Optional;
 
 import com.crud.java.model.model;
 import com.crud.java.repository.repo;
+
 @CrossOrigin(origins = "https://crud-spring.vercel.app")
 
 @RestController
 public class UserController {
     @Autowired
     public repo Repo;
+
     @PostMapping("/UserPost")
-    public String saveUser(@RequestBody model data){
+    public void saveUser(@RequestBody model data) {
         Repo.save(data);
-        return "Added User with id : " +data.getUserID();
     }
+
     @GetMapping("/")
-    public List<model>getList(){
+    public List<model> getList() {
         return Repo.findAll();
     }
-    
+
     @PutMapping("/UserUpdate/{id}")
-    public String updateUser(@PathVariable int id,@RequestBody model updatedUser){
-        Optional<model> data = Repo.findById(id);
-        if(data.isPresent()){
-            updatedUser.setUserID(id);
-            Repo.save(updatedUser);
-            return "Updated User with id : " + id;
-        } else {
-            return "User with id : " + id + " not found";
-        }
+public void updateUser(@PathVariable int id, @RequestBody model updatedUser) {
+    Optional<model> data = Repo.findById(id);
+    if (data.isPresent()) {
+        updatedUser.setUserID(id);
+        Repo.save(updatedUser);
     }
-  
+}
+
     @DeleteMapping("/UserDelete/{id}")
-    public String deleteUser(@PathVariable int id) {
+    public void deleteUser(@PathVariable int id) {
         Optional<model> data = Repo.findById(id);
         if (data.isPresent()) {
             Repo.deleteById(id);
-            return "Deleted User with id : " + id;
-        } else {
-            return "User with id : " + id + " not found";
-        }
-    }
-    
-}
+        } 
 
+    }}
